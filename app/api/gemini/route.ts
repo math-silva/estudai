528100ce3d8d0c9fc2b6ad4d0fe505c716c7e15b
Matheus Silva <matheus.souza28042001@gmail.com>
2024-05-14 18:09:49 -0300

    refactor: update gemini model

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
 
   if (API_KEY) {
     const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
