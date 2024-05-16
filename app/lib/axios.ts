8b3e0aefa118203df641a1a71bd33feb16eef957
Matheus Silva <matheus.souza28042001@gmail.com>
2024-05-16 11:14:59 -0300

    refactor: add no-cache config to api requests

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+axios.defaults.headers.common['Cache-Control'] = 'no-cache';
+
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL || "https://projetoestudai.vercel.app/api/",
   headers: {
